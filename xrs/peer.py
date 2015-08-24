02c7408b8812a4fd17a7e83e0f3ff79a305b59ff
RJB <rbirkner@student.ethz.ch>
2015-08-24 14:18:32 +0000

    fixed bug: route server gets notification when peers disconnects is unavailable

--- xrs/peer.py
+++ xrs/peer.py
@@ -15,6 +15,7 @@ class peer():
         
         self.asn = asn
         self.ports = ports
+        self.down = True
         
         ips = []
         for port in ports:
@@ -44,71 +45,83 @@ class peer():
         community = None
         
         route_list = []
-        
+   
         if ('state' in route['neighbor'] and route['neighbor']['state']=='down'):
             #TODO WHY NOT COMPLETELY DELETE LOCAL?
+            if LOG:
+                print "PEER DOWN - ASN "+str(self.asn)
+
             routes = self.rib['input'].get_all()
                         
             for route_item in routes:
-                self.rib['local'].delete(route_item['prefix'])
+                route_list.append({'withdraw': route_item})
+
+            self.rib['local'].delete_all()
             self.rib['local'].commit()
                       
+            self.rib['output'].delete_all()
+            self.rib['output'].commit()
+
             self.rib["input"].delete_all()
             self.rib["input"].commit()
-                        
-        if ('update' in route['neighbor']['message']):
-            if ('attribute' in route['neighbor']['message']['update']):
-                attribute = route['neighbor']['message']['update']['attribute']
+
+        if ('message' in route['neighbor']):
+            if LOG: 
+                print "RECEIVED UPDATE - ASN "+str(self.asn)
+
+            if ('update' in route['neighbor']['message']):
+                if ('attribute' in route['neighbor']['message']['update']):
+                    attribute = route['neighbor']['message']['update']['attribute']
                             
-                origin = attribute['origin'] if 'origin' in attribute else ''
+                    origin = attribute['origin'] if 'origin' in attribute else ''
                             
-                temp_as_path = attribute['as-path'] if 'as-path' in attribute else []
+                    temp_as_path = attribute['as-path'] if 'as-path' in attribute else []
                 
-                temp_as_set = attribute['as-set'] if 'as-set' in attribute else []
+                    temp_as_set = attribute['as-set'] if 'as-set' in attribute else []
 
-                as_path = ' '.join(map(str,temp_as_path)).replace('[','').replace(']','').replace(',','')
-                if len(temp_as_set) > 0:
-                    as_path += ' ( '+' '.join(map(str,sorted(list(temp_as_set), key=int))).replace('[','').replace(']','').replace(',','')+' )'
+                    as_path = ' '.join(map(str,temp_as_path)).replace('[','').replace(']','').replace(',','')
+                    if len(temp_as_set) > 0:
+                        as_path += ' ( '+' '.join(map(str,sorted(list(temp_as_set), key=int))).replace('[','').replace(']','').replace(',','')+' )'
 
-                if LOG:
-                    print "AS PATH: " + str(attribute['as-path'] if 'as-path' in attribute else "empty")
-                    print "AS SET: " + str(attribute['as-set'] if 'as-set' in attribute else "empty")
-                    print "COMBINATION: " + str(as_path)
+                    if LOG:
+                        print "AS PATH: " + str(attribute['as-path'] if 'as-path' in attribute else "empty")
+                        print "AS SET: " + str(attribute['as-set'] if 'as-set' in attribute else "empty")
+                        print "COMBINATION: " + str(as_path)
                                             
-                med = attribute['med'] if 'med' in attribute else ''
+                    med = attribute['med'] if 'med' in attribute else ''
                             
-                community = attribute['community'] if 'community' in attribute else ''
-                communities = ''
-                for c in community:
-                    communities += ':'.join(map(str,c)) + " " 
+                    community = attribute['community'] if 'community' in attribute else ''
+                    communities = ''
+                    for c in community:
+                        communities += ':'.join(map(str,c)) + " " 
                                 
-                atomic_aggregate = attribute['atomic-aggregate'] if 'atomic-aggregate' in attribute else ''
+                    atomic_aggregate = attribute['atomic-aggregate'] if 'atomic-aggregate' in attribute else ''
                         
-            if ('announce' in route['neighbor']['message']['update']):
-                announce = route['neighbor']['message']['update']['announce']
-                if ('ipv4 unicast' in announce):
-                    for next_hop in announce['ipv4 unicast'].keys():
-                        for prefix in announce['ipv4 unicast'][next_hop].keys():
-                            self.rib["input"][prefix] = (next_hop,
-                                                         origin,
-                                                         as_path,
-                                                         communities,
-                                                         med,
-                                                         atomic_aggregate)
-                            self.rib["input"].commit()
-                            announce_route = self.rib["input"][prefix]
+                if ('announce' in route['neighbor']['message']['update']):
+                    announce = route['neighbor']['message']['update']['announce']
+                    if ('ipv4 unicast' in announce):
+                        for next_hop in announce['ipv4 unicast'].keys():
+                            for prefix in announce['ipv4 unicast'][next_hop].keys():
+                                self.rib["input"][prefix] = (next_hop,
+                                                             origin,
+                                                             as_path,
+                                                             communities,
+                                                             med,
+                                                             atomic_aggregate)
+                                self.rib["input"].commit()
+                                announce_route = self.rib["input"][prefix]
                                         
-                            route_list.append({'announce': announce_route})
+                                route_list.append({'announce': announce_route})
 
-            elif ('withdraw' in route['neighbor']['message']['update']):
-                withdraw = route['neighbor']['message']['update']['withdraw']
-                if ('ipv4 unicast' in withdraw):
-                    for prefix in withdraw['ipv4 unicast'].keys():
-                        deleted_route = self.rib["input"][prefix]
-                        self.rib["input"].delete(prefix)
-                        self.rib["input"].commit()
+                elif ('withdraw' in route['neighbor']['message']['update']):
+                    withdraw = route['neighbor']['message']['update']['withdraw']
+                    if ('ipv4 unicast' in withdraw):
+                        for prefix in withdraw['ipv4 unicast'].keys():
+                            deleted_route = self.rib["input"][prefix]
+                            self.rib["input"].delete(prefix)
+                            self.rib["input"].commit()
                                     
-                        route_list.append({'withdraw': deleted_route})
+                            route_list.append({'withdraw': deleted_route})
                                     
         return route_list
     
