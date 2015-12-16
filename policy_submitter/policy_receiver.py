3302aa96c387b985e0d3e115655f2a12a20a1464
Arpit Gupta <glex.qsd@gmail.com>
2015-12-16 18:01:03 -0500

    Added the script to parse the policy file

---
+++ policy_submitter/policy_receiver.py
@@ -0,0 +1,36 @@
+#  Author:
+#  Arpit Gupta (Princeton)
+
+import json
+from multiprocessing.connection import Listener
+
+POLICY_SOCKET = ("localhost", 5551)
+LOG = True
+
+def policy_listener():
+        '''Socket listener for policy events '''
+        if LOG: print "Policy Event Handler started."
+        listener_eh = Listener(POLICY_SOCKET, authkey=None)
+        while True:
+            if LOG: print "waiting for connection..."
+            conn_eh = listener_eh.accept()
+
+            tmp = conn_eh.recv()
+
+            if tmp != "terminate":
+                if LOG: print "established connection..."
+
+                data = json.loads(tmp)
+
+                if LOG: print "# of policy items received", len(data.values())
+
+                # Send a message back to the sender.
+                reply = "Policy items received"
+                conn_eh.send(reply)
+            conn_eh.close()
+
+
+
+''' main '''
+if __name__ == '__main__':
+    policy_listener()
